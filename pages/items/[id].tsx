6c1a3f2b5d219da0cdcc30d6c0cb976c390340f2
이태언 <itaeeon@itaeeon-ui-MacBookPro.local>
2022-04-20 05:43:47 +0900

    .5.5

--- pages/items/[id].tsx
+++ pages/items/[id].tsx
@@ -6,7 +6,7 @@ const ItemDetail: NextPage = () => {
   return (
     <div className="px-4 py-10">
       <div className="mb-8">
-        <div className="h-96 bg-slate-300" />
+        <div className="h-96 bg-slate-300" />        
         <div className="flex items-center py-3 mt-1 space-x-3 border-t border-b cursor-pointer">
           <div className="w-12 h-12 rounded-full bg-slate-300" />
           <div>
@@ -16,7 +16,7 @@ const ItemDetail: NextPage = () => {
             </p>
           </div>
         </div>
-        <div className="mt-5">
+        <div className="mt-8">
           <h1 className="text-3xl font-bold text-gray-900">Galaxy S50</h1>
           <span className="mt-3 text-3xl text-gray-900">$140</span>
           <p className="my-6 text-base text-gray-700">
@@ -54,13 +54,13 @@ const ItemDetail: NextPage = () => {
         </div>
       </div>
       <div>
-        <h2>Similar items</h2>
-        <div>
+        <h2 className="text-2xl font-bold text-gray-900">Similar items</h2>
+        <div className="grid grid-cols-2 gap-4">
           {[1, 2, 3, 4, 5, 6].map((_, i) => (
             <div key={i}>
-              <div />
-              <h3>Galaxy S60</h3>
-              <p>$6</p>
+              <div className="mt-6 h-56 w-full bg-slate-300" />
+              <h3 className="text-gray-700 -mb-1">Galaxy S60</h3>
+              <span className="text-sm font-medium text-gray-900">$6</span>
             </div>
           ))}
         </div>
