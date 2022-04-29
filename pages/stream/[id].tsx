84653ab90472caa8f04898ea7a457815837159b4
이태언 <mktears0609@gmail.com>
2022-04-29 22:16:13 +0900

    ADD: inside update

--- pages/stream/[id].tsx
+++ pages/stream/[id].tsx
@@ -5,9 +5,23 @@ const StreamDetail: NextPage = () => {
     <div>
       <div className="px-3 pt-10 space-y-4 ">
         <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
-        <h3 className="mt-2 text-2xl font-semibold text-gray-700">
-          {"Let's try potatos"}
-        </h3>
+        <div className="mt-2">
+          <h1 className="text-3xl font-bold text-gray-900">Galaxy S50</h1>
+          <span className="mt-6 text-xl text-gray-900">$140</span>
+          <p className="my-6 text-base text-gray-800">
+            My money&apos;s in that office, right? If she start giving me some
+            bullshit about it ain&apos;t there, and we got to go someplace else
+            and get it, I&apos;m gonna shoot you in the head then and there.
+            Then I&apos;m gonna shoot that bitch in the kneecaps, find out where
+            my goddamn money is. She gonna tell me too. Hey, look at me when
+            I&apos;m talking to you, motherfucker. You listen: we go in there,
+            and that ni**a Winston or anybody else is in there, you the first
+            motherfucker to get shot. You understand?
+          </p>
+        </div>
+      </div>
+      <div className="px-3">
+        <h3 className="text-xl font-bold">Live Chat</h3>
       </div>
       <div className="px-4 pb-16 mt-10 space-y-4 h-[50vh] overflow-y-scroll">
         <div className="flex items-center space-x-2">
