cb15f4a43e4211abbff35e826f10934f616f903b
이태언 <mktears0609@gmail.com>
2022-05-01 16:40:45 +0900

    ADD : layouy 감싸기 및 하단 탭바 생성

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { getClass } from "../libs/utils";
 
 interface LayoutProps {
@@ -15,19 +16,119 @@ export default function Layout({
 }: LayoutProps) {
   return (
     <div>
-      <div className="fixed top-0 flex items-center justify-center w-full py-3 text-lg font-medium text-gray-700 bg-white border-b">
+      <div className="fixed top-0 flex items-center justify-center w-full max-w-xl py-3 text-lg font-medium text-gray-700 bg-white border-b">
         {title && <span>{title}</span>}
       </div>
-      <div className={getClass("pt-8", hasTabBar ? "pb-16" : "")}>
+      <div className={getClass("pt-8", hasTabBar ? "pb-8" : "")}>
         {children}
       </div>
       {hasTabBar ? (
-        <nav className="fixed bottom-0 flex items-center justify-between pt-3 pb-10 text-gray-800 bg-white border-t">
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
+        <nav className="fixed bottom-0 flex justify-between w-full max-w-xl px-10 pt-3 pb-5 text-xs text-gray-700 bg-white border-t">
+          <Link href="/">
+            <a>
+              <div className="flex flex-col items-center space-y-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6 "
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                  />
+                </svg>
+                <span>홈</span>
+              </div>
+            </a>
+          </Link>
+          <Link href={"/community"}>
+            <a>
+              <div className="flex flex-col items-center space-y-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
+                  />
+                </svg>
+                <span>동네생활</span>
+              </div>
+            </a>
+          </Link>
+          <Link href={"/chats"}>
+            <a>
+              <div className="flex flex-col items-center space-y-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"
+                  />
+                </svg>
+                <span>채팅</span>
+              </div>
+            </a>
+          </Link>
+          <Link href={"/streams"}>
+            <a>
+              <div className="flex flex-col items-center space-y-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
+                  />
+                </svg>
+                <span>라이브</span>
+              </div>
+            </a>
+          </Link>
+          <Link href={"/profile"}>
+            <a>
+              <div className="flex flex-col items-center space-y-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                  />
+                </svg>
+                <span>프로필</span>
+              </div>
+            </a>
+          </Link>
         </nav>
       ) : null}
     </div>
--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,23 +1,26 @@
 import { NextPage } from "next";
+import Layout from "../../components/layout";
 
 const Chats: NextPage = () => {
   return (
-    <div className="py-10 divide-y-[1px]">
-      {[1, 2, 3, 4].map((_, i) => (
-        <div
-          key={i}
-          className="flex px-4 py-3 mt-1 space-x-3 cursor-pointer items-starts-center"
-        >
-          <div className="w-12 h-12 rounded-full bg-slate-300" />
-          <div>
-            <p className="text-gray-700 ">Steve Jebs</p>
-            <p className="text-sm font-medium text-gray-500">
-              See you tomorrow in the corner at 2pm!
-            </p>
+    <Layout title="채팅 목록" hasTabBar>
+      <div className="py-10 divide-y-[1px]">
+        {[1, 2, 3, 4].map((_, i) => (
+          <div
+            key={i}
+            className="flex px-4 py-3 mt-1 space-x-3 cursor-pointer items-starts-center"
+          >
+            <div className="w-12 h-12 rounded-full bg-slate-300" />
+            <div>
+              <p className="text-gray-700 ">Steve Jebs</p>
+              <p className="text-sm font-medium text-gray-500">
+                See you tomorrow in the corner at 2pm!
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </Layout>
   );
 };
 
