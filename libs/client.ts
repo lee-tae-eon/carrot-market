d0473363faf9405f5bb59a44ae1f9907f49e694a
이태언 <mktears0609@gmail.com>
2022-05-04 06:22:21 +0900

    ADD: api routes && adding user

--- libs/client.ts
+++ libs/client.ts
@@ -1,10 +1,4 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaClient = new PrismaClient();
-
-prismaClient.user.create({
-  data: {
-    email: "eon@naver.com",
-    name: "lee",
-  },
-});
+const client = new PrismaClient();
+export default client;
