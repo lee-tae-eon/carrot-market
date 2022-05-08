698674084982e496c863fec2b711d7d20ab08385
이태언 <mktears0609@gmail.com>
2022-05-08 17:30:57 +0900

    Form test

--- pages/form.tsx
+++ pages/form.tsx
@@ -4,6 +4,7 @@ export default function Forms() {
   const [userName, setUserName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [formErrors, setFormErrors] = useState("");
 
   const onUserNameChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
     const {
@@ -27,28 +28,42 @@ export default function Forms() {
     setPassword(value);
   };
 
+  const onSubmit = (event: React.SyntheticEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (userName === "" || email === "" || password === "") {
+      setFormErrors("all fields are required");
+    }
+  };
+
   console.log(userName, email, password);
 
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <input
         onChange={onUserNameChange}
         value={userName}
         type="text"
         placeholder="username"
+        required
+        minLength={5}
       />
       <input
         onChange={onEmailChange}
         value={email}
         type="email"
         placeholder="Email"
+        required
       />
       <input
         onChange={onPasswordChange}
         value={password}
         type="password"
         placeholder="Password"
+        required
       />
+      <button className="w-32 h-16 text-center" type="submit">
+        Create Account
+      </button>
     </form>
   );
 }
