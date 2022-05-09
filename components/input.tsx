61d236d9099f40f46fb075b57c6d6a1885e4b363
이태언 <mktears0609@gmail.com>
2022-05-09 20:13:26 +0900

    ADD: layer input  number css

--- components/input.tsx
+++ components/input.tsx
@@ -9,6 +9,7 @@ export default function Input({
   label,
   name,
   kind = "text",
+  register,
   ...rest
 }: InputProps) {
   return (
@@ -23,6 +24,7 @@ export default function Input({
         <div className="relative flex items-center rounded-md shadow-sm">
           <input
             id={name}
+            {...register}
             {...rest}
             className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-orange-500 focus:border-orange-500"
           />
@@ -35,6 +37,7 @@ export default function Input({
           </div>
           <input
             id={name}
+            {...register}
             {...rest}
             className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none pl-7 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
           />
@@ -50,8 +53,9 @@ export default function Input({
           </span>
           <input
             id={name}
+            {...register}
             {...rest}
-            className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md rounded-l-none shadow-sm appearance-none focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+            className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md rounded-l-none shadow-sm appearance-none focus:outline-none focus:ring-orange-500 focus:border-orange-500 "
           />
         </div>
       ) : null}
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,12 +1,31 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import Button from "../components/button";
 import Input from "../components/input";
 import { getClass } from "../libs/utils";
 
+interface EnterForm {
+  email?: string;
+  phone?: string;
+}
+
 export default function Enter() {
+  const { register, watch, handleSubmit, reset } = useForm<EnterForm>();
+
   const [method, setMethod] = useState<"email" | "phone">("email");
-  const onEmailClick = () => setMethod("email");
-  const onPhoneClick = () => setMethod("phone");
+  const onEmailClick = () => {
+    reset();
+    setMethod("email");
+  };
+  const onPhoneClick = () => {
+    reset();
+    setMethod("phone");
+  };
+
+  const onValid = (data: EnterForm) => {
+    console.log(data);
+  };
+
   return (
     <div className="p-5 mt-16">
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
@@ -38,12 +57,22 @@ export default function Enter() {
             </button>
           </div>
         </div>
-        <form className="flex flex-col mt-8 space-y-4">
+        <form
+          onSubmit={handleSubmit(onValid)}
+          className="flex flex-col mt-8 space-y-4"
+        >
           {method === "email" ? (
-            <Input name="email" label="Email address" type="email" required />
+            <Input
+              register={register("email")}
+              name="email"
+              label="Email address"
+              type="email"
+              required
+            />
           ) : null}
           {method === "phone" ? (
             <Input
+              register={register("phone")}
               name="phone"
               label="Phone number"
               type="number"
