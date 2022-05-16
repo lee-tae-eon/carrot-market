3cbbd9be901abd4cb91179ab73281da55efae062
이태언 <mktears0609@gmail.com>
2022-05-16 23:52:54 +0900

    TEST: test

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -2,7 +2,14 @@ import { withIronSessionApiRoute } from "iron-session/next";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
-
+// declar module
+declare module "iron-session" {
+  interface IronSessionData {
+    user?: {
+      id: number;
+    };
+  }
+}
 async function handler(req: NextApiRequest, res: NextApiResponse<ResType>) {
   const { token } = req.body;
   const exists = await client.token.findUnique({
@@ -11,10 +18,10 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResType>) {
     },
     include: { user: true },
   });
-  if (!exists) res.status(404).end();
+  if (!exists) return res.status(404).end();
 
   req.session.user = {
-    id: exists?.userId,
+    id: exists.userId,
   };
   await req.session.save();
   res.status(200).end();
