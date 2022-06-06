457ad1701c60c3048a805c8751f085a2a5155173
이태언 <mktears0609@gmail.com>
2022-06-06 21:33:47 +0900

    ADD: write useForm

--- pages/community/write.tsx
+++ pages/community/write.tsx
@@ -2,13 +2,32 @@ import type { NextPage } from "next";
 import Button from "@components/button";
 import Layout from "@components/layout";
 import TextArea from "@components/textarea";
+import { useForm } from "react-hook-form";
+import useMutation from "@libs/client/useMutation";
+import { useEffect } from "react";
+
+interface WriteFormProps {
+  question: string;
+}
 
 const Write: NextPage = () => {
+  const { register, handleSubmit } = useForm<WriteFormProps>();
+  const [post, { loading, data }] = useMutation("/api/posts");
+  const onValid = (data: WriteFormProps) => {
+    if (loading) return;
+    post(data);
+  };
+
+  useEffect(() => {}, [data]);
   return (
     <Layout canGoBack title="Write Post">
-      <form className="p-4 space-y-4">
-        <TextArea required placeholder="Ask a question!" />
-        <Button text="Submit" />
+      <form onSubmit={handleSubmit(onValid)} className="p-4 space-y-4">
+        <TextArea
+          register={register("question", { required: true, minLength: 5 })}
+          required
+          placeholder="Ask a question!"
+        />
+        <Button text={loading ? "Loading..." : "Submit"} />
       </form>
     </Layout>
   );
