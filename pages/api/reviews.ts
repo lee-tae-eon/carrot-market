f9757c9dc623471050d1cea1a6de0d0440e3e8f7
이태언 <mktears0609@gmail.com>
2022-06-15 22:38:25 +0900

    ADD: review api && review schema

---
+++ pages/api/reviews.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResType>) {
+  const {
+    session: { user },
+  } = req;
+
+  const reviews = await client.review.findMany({
+    where: {
+      createdForId: user?.id,
+    },
+  });
+
+  res.json({
+    ok: true,
+    reviews,
+  });
+}
+// * nextjs 가 excute할 껍데기 handler
+export default withApiSession(
+  withHandler({
+    method: ["GET"],
+    handler,
+  })
+);
