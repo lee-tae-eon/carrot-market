c5552e405552b2cf8412410d2f9559ea3bc16ea6
이태언 <mktears0609@gmail.com>
2022-06-20 22:59:07 +0900

    ADD: sales, favs, purchases api

---
+++ pages/api/users/me/purchases.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResType>) {
+  const {
+    session: { user },
+  } = req;
+
+  const purchases = await client.purchase.findMany({
+    where: {
+      userId: user?.id,
+    },
+  });
+
+  res.json({
+    ok: true,
+    purchases,
+  });
+}
+// * nextjs 가 excute할 껍데기 handler
+export default withApiSession(
+  withHandler({
+    method: ["GET"],
+    handler,
+  })
+);
---
+++ pages/api/users/me/sales.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResType>) {
+  const {
+    session: { user },
+  } = req;
+
+  const sales = await client.sale.findMany({
+    where: {
+      userId: user?.id,
+    },
+  });
+
+  res.json({
+    ok: true,
+    sales,
+  });
+}
+// * nextjs 가 excute할 껍데기 handler
+export default withApiSession(
+  withHandler({
+    method: ["GET"],
+    handler,
+  })
+);
