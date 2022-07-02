2ae38b6450d4fd0889cf6c5ef7d7c6e0e5d1d44d
이태언 <mktears0609@gmail.com>
2022-07-02 21:23:47 +0900

    ADD: useForm create stream

--- pages/streams/create.tsx
+++ pages/streams/create.tsx
@@ -3,21 +3,40 @@ import Input from "@components/input";
 import Layout from "@components/layout";
 import TextArea from "@components/textarea";
 import type { NextPage } from "next";
+import { useForm } from "react-hook-form";
+
+interface CreateForm {
+  name: string;
+  price: string;
+  description: string;
+}
 
 const Create: NextPage = () => {
+  const { handleSubmit, register } = useForm();
   return (
     <Layout canGoBack title="Go Live">
       <form className="px-4 py-10 space-y-4 ">
-        <Input required label="Name" name="name" type="text" />
         <Input
+          register={register("name")}
+          required
+          label="Name"
+          name="name"
+          type="text"
+        />
+        <Input
+          register={register("price")}
           required
           label="Price"
-          placeholder="0.00"
+          // placeholder="0.00"
           name="price"
           type="text"
           kind="price"
         />
-        <TextArea name="description" label="Description" />
+        <TextArea
+          register={register("description")}
+          name="description"
+          label="Description"
+        />
         <Button text="Go live" />
       </form>
     </Layout>
