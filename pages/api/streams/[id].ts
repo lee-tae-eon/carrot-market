159516328bdbb77773b1166b59e89b66b97370fc
이태언 <mktears0609@gmail.com>
2022-07-04 22:03:57 +0900

    ADD: get stream api

---
+++ pages/api/streams/[id].ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResType>) {
+  const {
+    query: { id },
+  } = req;
+
+  const stream = await client.stream.findUnique({
+    where: {
+      id: +id.toString(),
+    },
+  });
+  res.json({ ok: true, stream });
+}
+// * nextjs 가 excute할 껍데기 handler
+export default withApiSession(
+  withHandler({
+    method: ["GET"],
+    handler,
+  })
+);
