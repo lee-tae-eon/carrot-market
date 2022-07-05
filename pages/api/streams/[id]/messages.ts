47636640293b36b8392facf6403c934103a1baf0
이태언 <mktears0609@gmail.com>
2022-07-05 22:18:56 +0900

    ADD: message post api

---
+++ pages/api/streams/[id]/messages.ts
@@ -0,0 +1,36 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResType>) {
+  const {
+    query: { id },
+    body,
+    session: { user },
+  } = req;
+
+  const message = await client.message.create({
+    data: {
+      message: body.message,
+      stream: {
+        connect: {
+          id: +id.toString(),
+        },
+      },
+      user: {
+        connect: {
+          id: user?.id,
+        },
+      },
+    },
+  });
+  res.json({ ok: true, message });
+}
+// * nextjs 가 excute할 껍데기 handler
+export default withApiSession(
+  withHandler({
+    method: ["POST"],
+    handler,
+  })
+);
