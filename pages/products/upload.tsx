6a3735cb31d1ca1236fdbcc3116ecaee47144448
이태언 <mktears0609@gmail.com>
2022-07-16 22:11:10 +0900

    ADD: upload item image cloud flare

--- pages/products/upload.tsx
+++ pages/products/upload.tsx
@@ -6,7 +6,7 @@ import Input from "@components/input";
 import Layout from "@components/layout";
 import TextArea from "@components/textarea";
 import useMutation from "@libs/client/useMutation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Product } from "@prisma/client";
 import { useRouter } from "next/router";
 
@@ -24,15 +24,25 @@ interface UploadProductMutation {
 
 const Upload: NextPage = () => {
   const router = useRouter();
-  const { register, handleSubmit } = useForm<UploadProductForm>({
+  const { register, handleSubmit, watch } = useForm<UploadProductForm>({
     mode: "onChange",
   });
   const [uploadProduct, { loading, data }] =
     useMutation<UploadProductMutation>("/api/products");
 
-  const onValid = (data: UploadProductForm) => {
+  const onValid = async ({ name, price, description }: UploadProductForm) => {
     if (loading) return;
-    uploadProduct(data);
+    if (photo && photo.length > 0) {
+      const { uploadURL } = await (await fetch(`/apo/files`)).json();
+      const form = new FormData();
+      form.append("file", photo[0], name);
+      const {
+        result: { id },
+      } = await (await fetch(uploadURL, { method: "POST", body: form })).json();
+      uploadProduct({ name, price, description, photoId: id });
+    } else {
+      uploadProduct({ name, price, description });
+    }
   };
 
   useEffect(() => {
@@ -40,32 +50,50 @@ const Upload: NextPage = () => {
       router.push(`/products/${data.product.id}`);
     }
   }, [data, router]);
+
+  const [photoPrev, setPhotoPrev] = useState("");
+  const photo = watch("photo");
+
+  useEffect(() => {
+    if (photo && photo.length > 0) {
+      const file = photo[0];
+      setPhotoPrev(URL.createObjectURL(file));
+    }
+  }, [photo]);
+
   return (
     <Layout canGoBack title="Upload Product">
       <form onSubmit={handleSubmit(onValid)} className="p-4 space-y-4">
         <div>
-          <label className="flex items-center justify-center w-full h-48 text-gray-600 border-2 border-gray-300 border-dashed rounded-md cursor-pointer hover:border-orange-500 hover:text-orange-500">
-            <svg
-              className="w-12 h-12"
-              stroke="currentColor"
-              fill="none"
-              viewBox="0 0 48 48"
-              aria-hidden="true"
-            >
-              <path
-                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-            <input
-              {...register("photo")}
-              accept="image/*"
-              className="hidden"
-              type="file"
+          {photoPrev ? (
+            <img
+              src={photoPrev}
+              className="w-full text-gray-600 rounded-md h-46 aspect-video"
             />
-          </label>
+          ) : (
+            <label className="flex items-center justify-center w-full h-48 text-gray-600 border-2 border-gray-300 border-dashed rounded-md cursor-pointer hover:border-orange-500 hover:text-orange-500">
+              <svg
+                className="w-12 h-12"
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 48 48"
+                aria-hidden="true"
+              >
+                <path
+                  d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+              <input
+                {...register("photo")}
+                accept="image/*"
+                className="hidden"
+                type="file"
+              />
+            </label>
+          )}
         </div>
         <Input
           register={register("name", {
