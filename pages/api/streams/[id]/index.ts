93712be4f0662195b86444601f4c329b5ed4d912
이태언 <mktears0609@gmail.com>
2022-07-25 22:37:52 +0900

    FIX: GET stream api modify is Owner

--- pages/api/streams/[id]/index.ts
+++ pages/api/streams/[id]/index.ts
@@ -6,12 +6,14 @@ import { withApiSession } from "@libs/server/withSession";
 async function handler(req: NextApiRequest, res: NextApiResponse<ResType>) {
   const {
     query: { id },
+    session: { user },
   } = req;
 
   const stream = await client.stream.findUnique({
     where: {
       id: +id.toString(),
     },
+    // * includes 는 모든정보를 노출  select로 선택하자
     include: {
       messages: {
         select: {
@@ -27,6 +29,17 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResType>) {
       },
     },
   });
+  if (!stream) {
+    return res.status(404).json({ ok: false, message: "nothing" });
+  }
+
+  const isOwner = stream?.userId === user?.id;
+
+  if (stream && !isOwner) {
+    stream.cloudflareKey = "xxxx";
+    stream.cloudflareUrl = "xxxx";
+  }
+
   res.json({ ok: true, stream });
 }
 // * nextjs 가 excute할 껍데기 handler
