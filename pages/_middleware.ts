9bcc583251d133019b45038ceeabf41c67853b1c
이태언 <mktears0609@gmail.com>
2022-07-29 21:48:36 +0900

    ADD: middleware response

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,5 +1,13 @@
 import type { NextFetchEvent, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  console.log(req.ua);
+  if (req.ua?.isBot) {
+    return new Response("로봇은 안됩니다.", { status: 403 });
+  }
+  if (!req.url.includes("/api")) {
+    if (!req.url.includes("/enter") && !req.cookies.carrotsession) {
+      return NextResponse.redirect(`${req.nextUrl.origin}/enter`);
+    }
+  }
 }
