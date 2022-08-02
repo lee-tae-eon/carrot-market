94abc9e558cf08fd3afd9ad9a0d81aeae9aeb0be
이태언 <mktears0609@gmail.com>
2022-08-02 19:43:52 +0900

    ADD: font family google in document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,12 @@ class CustomDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="ko">
-        <Head />
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Mouse+Memoirs&family=Oswald:wght@300&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
