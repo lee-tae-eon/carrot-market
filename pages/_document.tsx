b14853349bd7c71fc4dbcbe53940ba316e141104
이태언 <mktears0609@gmail.com>
2022-08-04 22:19:33 +0900

    ADD: font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ class CustomDocument extends Document {
       <Html lang="ko">
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Mouse+Memoirs&family=Oswald:wght@300&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap"
             rel="stylesheet"
           />
         </Head>
