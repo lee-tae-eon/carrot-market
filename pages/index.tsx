eb5605642148f4dac341dea61b8c4ed95b663390
이태언 <mktears0609@gmail.com>
2022-08-08 21:55:58 +0900

    FIX: warning

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type { GetServerSideProps, NextPage } from "next";
+import type { NextPage } from "next";
 
 import FloatingButton from "@components/floating-button";
 import Item from "@components/item";
