540bf8ec60f6fc2fbe22ea8fa208035143517532
이태언 <mktears0609@gmail.com>
2022-08-10 22:47:53 +0900

    ADD: blog page

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -3,7 +3,10 @@ import Layout from "@components/layout";
 export default function Blog() {
   return (
     <Layout title="Blog">
-      <h1 className="mt-10 text-lg font-semibold">first Post</h1>
+      <h1 className="mt-10 text-lg font-semibold">First Post:</h1>
+      <ul>
+        <li>Welcome everyone!</li>
+      </ul>
     </Layout>
   );
 }
