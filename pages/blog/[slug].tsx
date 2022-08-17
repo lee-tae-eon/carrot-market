82bd46d9b90c15db0b87dc253ca7b641dfa1e34b
이태언 <mktears0609@gmail.com>
2022-08-17 22:14:58 +0900

    getstaticpaths test

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,8 +1,22 @@
+import { readdirSync } from "fs";
 import { NextPage } from "next";
 
 const Post: NextPage = () => {
   return <h1>h1</h1>;
 };
+
+//* getStaticProps 를 사용하는 페이지가 동적 페이지일때 사용하는 getStaticPaths
+export function getStaticPaths() {
+  const files = readdirSync("./posts/blogPost").map((file) => {
+    const [name, _] = file.split(".");
+    return { params: { slug: name } };
+  });
+
+  return {
+    paths: files,
+    fallback: false,
+  };
+}
 
 export function getStaticProps() {
   return {
